3700d99d645375f1ff537f42396f792740481b58
Nithin Mahesh <nmahesh@umass.edu>
2023-02-15 18:47:47 -0500

    Committing code changes, before messing with file structure to get it working with idea.exe

--- src/main/java/com/InstaFollower/Main.java
+++ src/main/java/com/InstaFollower/Main.java
@@ -11,13 +11,24 @@ public class Main {
         System.setProperty("webdriver.chrome.driver", "D:\\MyGit\\InstaFollower\\src\\main\\java\\com\\WebDriver\\chromedriver-110-0-5481-77.exe");
         WebDriver driver = new ChromeDriver();
 
+
         // Open the desired webpage
         driver.get("https://mail.tutanota.com/login?noAutoLogin=true");
 
-        driver.findElement(By.id("//*[@id=\"login-view\"]/div[2]/div/div[1]/form/div[1]/div/div/div/div/div/input"));
+        driver.findElement(By.xpath("//*[@id=\"login-view\"]/div[2]/div/div[3]/div/button")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//*[@id=\"login-view\"]/div[2]/div/div[4]/div/div/div/button[1]/div")).click();
 
-        // Close the driver instance
-        //Thread.sleep(60000);
-        driver.quit();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("/html/body/div/div[2]/div/div/div/div/div/div[2]/div/div/div[2]/div[2]/div[1]/div/div[1]/div[5]/button/div")).click();
+
+//        try {
+//            Thread.sleep(3000);
+            driver.findElement(By.xpath("'//*[@id=\"modal\"]/div[2]/div/div/div/div[2]/div[1]/div/input"));
+
+//        }
+//        catch(Exception e) {
+//            System.out.println("Failed");
+//        }
     }
 }
